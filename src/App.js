26d21169e2cee5a7ef84602ca0f8e029f0532f16
Samuel Vandamme <samuel@sava.be>
2021-03-11 12:01:37 +0100

    feat: added cookie consent

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, HashRouter } from 'react-router-dom';
 import Home from './Pages/Home';
 import View from './Pages/View';
+import CookieConsent from 'react-cookie-consent';
 import './style.scss';
 
 class App extends React.Component {
@@ -39,6 +40,31 @@ class App extends React.Component {
     }
     return (
       <HashRouter>
+        <CookieConsent buttonText="Yes">
+          <p>
+            This site uses cookies{' '}
+            <span role="img" aria-label="a yummie cookie">
+              🍪
+            </span>
+          </p>
+          <p>
+            We use cookies and other similar technologies ("Cookies") on our
+            websites and services to enhance your experience and to provide you
+            with relevant content. By using our websites and services you are
+            agreeing to the use of cookies.
+          </p>
+          <p>
+            You can read more{' '}
+            <a
+              href="https://newrelic.com/termsandconditions/cookie-policy"
+              target="_BLANK"
+              rel="noopener noreferrer"
+            >
+              here
+            </a>
+            . If you consent to our cookies, please click Yes.
+          </p>
+        </CookieConsent>
         <main role="main">
           <Switch>
             <Route
