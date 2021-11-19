85019d54fecf375ef8651788ab28a16bf39d13a8
Phil Weber <phil@philweber.com>
2021-11-18 23:47:55 -0800

    Added new quickstart

---
+++ library/BrokenLinkChecker/script.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+const $http = require('request');
+const parse = require('url-parse');
+const By = $driver.By;
+
+// Replace with the URL you want to check for broken links
+const urlToCheck = 'https://www.example.com/';
+
+$browser.get(urlToCheck)
+  .then(function() {
+    // find all 'a' elements
+    return $browser.findElements(By.tagName('a'));
+  })
+  .then(function(links) {
+    // for each link...
+    return links.forEach(function(link) {
+      // get link target
+      return link.getAttribute('href')
+        .then(function(href) {
+          // if href is not missing or empty...
+          if (href != null && href != '') {
+            // skip mailto and tel links
+            if (!href.startsWith('mailto') && !href.startsWith('tel')) {
+              var domain = parse(href).hostname;
+              // if target is not in list of excluded domains...
+              if (domain != 'www.instagram.com' && domain != 'www.linkedin.com') {
+                // send HEAD request and check response
+                return $http.head(href, callback);
+              }
+            }
+          }
+        });
+    });
+  });
+
+  function callback(error, response, body) {
+    if (response != null) {
+      assert.ok(response.statusCode != 404, 'Broken link found: ' + response.request.uri.href);
+    }
+  }
