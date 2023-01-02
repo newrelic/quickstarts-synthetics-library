3b54c6e061c43dd7e45a1e6be43c693f23e19b3f
kidk <samuel@sava.be>
2023-01-02 10:13:13 +0000

    Deploying to gh-pages from  @ dd0db053ad3506e13c4dd2ed113eb7f5e909ad4d 🚀

--- data/2FAOrMFA/script.js
+++ data/2FAOrMFA/script.js
@@ -1,17 +1,20 @@
-var speakeasy = require('speakeasy');
+var otpauth = require('otpauth');
 
 // The secret is the value obtained while setting up 2FA/MFA for your user and choosing the Can't scan the QR code option.
 // Capture this key and store it in a Secure Credential for use during monitor execution.
 var secret = $secure.TESTOKTAMFA;
 
 // This encoding and algorithm combination is used for Google Authenticator or Okta based TOTP.
-var MFAOptions = {
-  secret: secret,
-  encoding: 'base32',
-  algorithm: 'sha1'
-}
+let totp = new otpauth.TOTP({
+  algorithm: 'SHA1',
+  digits: 6,
+  period: 30,
+  secret: secret
+})
 
 // The scripted browser logic to navigate to the MFA screen would be located here. Find the object and then use this as the input to sendKeys.
 // Since this is time based, send the output to sendKeys directly instead of storing this in a variable at the top of your script.
-// Example: el.sendKeys(speakeasy.totp(MFAOptions)) where el is the element.
-speakeasy.totp(MFAOptions)
+// Example: el.sendKeys(totp.generate()) where el is the element.
+// This quickstart logs out the token instead of sending it to an element or as input to an API test.
+let token = totp.generate();
+console.log(token);
